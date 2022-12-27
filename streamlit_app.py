4569ef6f1ed6522181556bc919d5a08b641fa1c4
JAG <105790519+jairoalonso1@users.noreply.github.com>
2022-12-27 15:32:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ Shown are the stock closing price and volume of Google!
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = st.text_input('Googl', 'Aapl')
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
