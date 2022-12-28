8f5e063ae2da7ac160bc4ae7417bed6df85d849a
JAG <105790519+jairoalonso1@users.noreply.github.com>
2022-12-28 13:08:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.sidebar.title(APP_NAME)
 TICKERS = ['FB', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'MSFT']
 
 # Select ticker
-ticker = st.sidebar.selectbox('Select ticker', sorted(TICKERS), index=0)
+ticker = st.sidebar.selectbox('Accion o ETF', index=0)
 tickerData = yf.Ticker(ticker)
 
 # Set start and end point to fetch data
