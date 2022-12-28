7615ea35596279554077f780757445fa6fd2cafe
JAG <105790519+jairoalonso1@users.noreply.github.com>
2022-12-28 13:14:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.sidebar.title(APP_NAME)
 TICKERS = ['FB', 'AMZN', 'AAPL', 'NFLX', 'GOOGL', 'MSFT']
 
 # Select ticker
-ticker = st.sidebar.selectbox('Stock or ETF', TICKERS)
+ticker = st.text_input('')
 tickerData = yf.Ticker(ticker)
 
 # Set start and end point to fetch data
